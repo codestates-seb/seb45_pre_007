905cd334cfbe3e7cd49f1ee5c9fb25f59383b7a0
심도연 <shimdokite@gmail.com>
2023-08-10 17:38:31 +0900

    Feat: client 초기 세팅
    - cra로 React 라이브러리 시작
    - 필요한 라이브러리 설치
    - eslintrc, prettierrc 환경 설정
    
    Related to: #15

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import "./App.css";
+
+function App() {
+  const test = () => {
+    console.log("hello, world");
+    return;
+  };
+  return <>App</>;
+}
+
+export default App;
