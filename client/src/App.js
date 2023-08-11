705f0cc4f614ee3418b24b5257f0dd0470805d76
심도연 <shimdokite@gmail.com>
2023-08-11 16:30:06 +0900

    feat: react-reponsive 설치

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,17 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const App = () => {
-  return <>App</>;
+  const [data, setData] = useState('');
+  const url = process.env.REACT_APP_API_URL;
+
+  useEffect(() => {
+    axios
+      .get(`${url}/questions`)
+      .then((response) => setData(response.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return <>{data}</>;
 };
 
 export default App;
