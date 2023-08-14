e2dae3e675a55fc3133568f1f8821bb7e3b5526b
JongHoSke <astra9706@naver.com>
2023-08-14 16:00:55 +0900

    merge conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import Main from './pages/Main.jsx';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Header } from './components/Header.jsx';
+import { Header } from './components/header/Header.jsx';
 import { Login } from './pages/Login.jsx';
 import Footer from './components/Footer.jsx';
+import Ask from './pages/Ask.jsx';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Main />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/ask" element={<Ask />}></Route>
       </Routes>
       <Footer />
     </>
---
+++ server/src/main/java/com/lucky7/preproject/question/dto/responseDto/SingleQuestionResponseDto.java
@@ -0,0 +1,18 @@
+package com.lucky7.preproject.question.dto.responseDto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class SingleQuestionResponseDto {
+    private long questionId;
+    private String questionTitle;
+    private String questionContent;
+    private String questionUser;
+    private String createdAt;
+    private String lastModifiedAt;
+    private List<CommentDto> questionComments;   // CommentDto 생성 후에 담아줄 예정
+}
