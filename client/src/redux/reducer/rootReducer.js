6e505b9c056cb412c62406d3b8d083da12da8df4
jinyong1015 <jjyim1015@gmail.com>
2023-08-18 18:09:39 +0900

    feat: counter 기능 추가

--- client/src/redux/reducer/rootReducer.js
+++ client/src/redux/reducer/rootReducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from '@reduxjs/toolkit';
 import { askSlice } from '../feature/askSlice';
 import { loginSlice } from '../feature/loginSlice';
 import { counterSlice } from '../feature/counterSlice';
-// 아래에 createSlice import 해오기
+// 아래에 createSlice import 해오기!
 
 const rootReducer = combineReducers({
   ask: askSlice.reducer,
