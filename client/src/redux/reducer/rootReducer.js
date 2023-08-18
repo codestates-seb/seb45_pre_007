27270fa4e050e2c8aa933be8020bc1ee9b68477e
jinyong1015 <jjyim1015@gmail.com>
2023-08-18 17:31:24 +0900

    test

--- client/src/redux/reducer/rootReducer.js
+++ client/src/redux/reducer/rootReducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { lawsSlice } from '../feature/lawSlice';
 import { counterSlice } from '../feature/counterSlice';
-// 아래에 createSlice import 해오기
+// 아래에 createSlice import 해오기!
 
 const rootReducer = combineReducers({
   laws: lawsSlice.reducer,
